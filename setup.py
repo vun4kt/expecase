0d2be7a0090b98fe18f903e31c7caad434321af2
vun4kt <vun4kt@virginia.edu>
2024-03-26 23:56:34 -0400

    DEMO

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'demo',
+    version = '0.1',
+    description = 'A demo package',
+    url = 'https://github.com/vun4kt/expecase',
+    author = 'E. de la Vega Ricardo',
+    author_email = 'vun4kt@virginia.edu',
+    license = 'MIT',
+    packages = ['demo']
+)
